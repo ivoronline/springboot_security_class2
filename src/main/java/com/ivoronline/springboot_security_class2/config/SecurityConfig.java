b3ee973be3db4caa609f4bb7a14d4ecb93a55970
igoreta <ivoronline@gmail.com>
2021-01-16 10:10:59 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_security_class2/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.ivoronline.springboot_security_class2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    auth
+      .inMemoryAuthentication()
+      .withUser("myadmin").password("{noop}myadminpassword").roles("ADMIN")
+      .and()
+      .withUser("myuser").password("{noop}myuserpassword").roles("USER");
+
+    auth.inMemoryAuthentication().withUser("myuser2").password("{noop}myuser2password").roles("USER");
+
+  }
+
+}
+
+
---
+++ src/test/java/com/ivoronline/springboot_security_class2/SpringbootSecurityClass2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_security_class2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityClass2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
